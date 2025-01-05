8c50656a148fad707eeaed7da1d1572561ef6f7a
GPU Governor <gpu.governor@gmail.com>
2025-01-05 20:25:12 +0100

    added create window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include"unixform.h"
 int main() {
-
+xiCreateWindow();
+xiUpdate();
+xiDestroyWindow();
     return 0;
 }
--- src/unixform.h
+++ src/unixform.h
@@ -2,52 +2,40 @@
 #include <X11/Xlib.h> /// X11/XLIB
 #include<stdio.h> /// stdio.h 
 #include<stdlib.h> /// stdlib.h
+#include<stdbool.h>
 /// in your example program you may choose to include the above and it will work fine, but they are already done for you, and it may slow down your program due to multiple re-including
 
 ///------------------------- GLOBAL VAR -------------------------
+Display *xiDisplay;
+int xiScreen;
+Window xiWindow;
+XEvent xiEvent;
 ///------------------------- FUNCTIONS -------------------------
 int xiCreateWindow(){
     // Step 1: Open a connection to the X server
-    Display *display = XOpenDisplay(NULL);
-    if (!display) {
+    xiDisplay = XOpenDisplay(NULL);
+    if (!xiDisplay) {
         fprintf(stderr, "Unable to open X display\n");
         exit(1);
     }
 
     // Step 2: Create a window
-    int screen = DefaultScreen(display);
-    Window win = XCreateSimpleWindow(display, RootWindow(display, screen),10, 10, 500, 300, 1,BlackPixel(display, screen),WhitePixel(display, screen));
-	
+    xiScreen = DefaultScreen(xiDisplay);
+    xiWindow = XCreateSimpleWindow(xiDisplay, RootWindow(xiDisplay, xiScreen),10, 10, 500, 300, 1,BlackPixel(xiDisplay, xiScreen),WhitePixel(xiDisplay, xiScreen));
+    // Step 4: Make the window visible
+    XMapWindow(xiDisplay, xiWindow);
+
 }
 
 void xiDestroyWindow(){
 //  Clean up and close
-    XCloseDisplay(display);
+	XUnmapWindow(xiDisplay, xiWindow);
+	XDestroyWindow(xiDisplay, xiWindow);
+    XCloseDisplay(xiDisplay);
 }
-int main() {
 
-    // Step 3: Select events to handle
-    XSelectInput(display, win, ExposureMask | KeyPressMask);
-
-    // Step 4: Make the window visible
-    XMapWindow(display, win);
-
-    // Step 5: Event loop
-    XEvent event;
-    while (1) {
-        XNextEvent(display, &event);
-
-        if (event.type == Expose) {
-            // Draw a rectangle when the window is exposed
-            GC gc = XCreateGC(display, win, 0, NULL);
-            XSetForeground(display, gc, BlackPixel(display, screen));
-            XFillRectangle(display, win, gc, 50, 50, 200, 100);
-            XFreeGC(display, gc);
-        } else if (event.type == KeyPress) {
-            // Exit on key press
-            break;
-        }
-    }
-
-    return 0;
+void xiUpdate(){
+	while(true){
+		
+	}
 }
